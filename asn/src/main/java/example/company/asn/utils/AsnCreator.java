762e155298da982159460d1fdc483771fa54c99f
xavierdpt <xavierdpt>
2018-05-24 21:44:36 +0530

    .

---
+++ asn/src/main/java/example/company/asn/utils/AsnCreator.java
@@ -0,0 +1,12 @@
+package example.company.asn.utils;
+
+public class AsnCreator {
+
+	private AsnCreator() {
+	}
+
+	public static AsnCreator newInstance() {
+		return new AsnCreator();
+	}
+
+}
---
+++ fiddle/src/test/java/example/company/jse/fiddle/Fiddle8.java
@@ -0,0 +1,22 @@
+package example.company.jse.fiddle;
+
+import java.security.NoSuchAlgorithmException;
+
+import org.junit.Test;
+
+import example.company.asn.utils.AsnCreator;
+import example.company.asn.utils.AsnInterpretor;
+
+/* TODO : create an Asn structure and sign it with the private key in the key store such that it is byte-equals to the existing certificate */
+public class Fiddle8 {
+
+	@Test
+	public void fiddle() throws NoSuchAlgorithmException {
+		AsnInterpretor.class.getName();
+		
+		AsnCreator.newInstance();
+		
+	
+	}
+
+}
